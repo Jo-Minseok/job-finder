ab8c666379ead330aedd9f35308825d7e48f09fc
J_Coder <seok3765@naver.com>
2023-11-07 14:33:27 +0900

    20231107.14:33

--- Java/DataBaseProgramming/src/App.java
+++ Java/DataBaseProgramming/src/App.java
@@ -1,5 +1,21 @@
+import java.sql.*;
+
 public class App {
+
+public static void oracleConnection(
+        String db_driver,
+        String db_type,
+        String db_driver_type,
+        String db_host_name,
+        String db_posrt,
+        String db_service_name,
+        String db_username,
+        String db_password){
+            String Connection_string = String.format("%s:%s:%s:@%s:%s/%s",db_driver, db_type,db_driver_type, db_host_name, db_posrt, db_service_name);
+            System.out.println(Connection_string);
+        }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        oracleConnection("jdbc", "oracle", "thin", "minseok821lab.kro.kr", "3306", "orcl", "seok3764", "0424");
     }
 }
