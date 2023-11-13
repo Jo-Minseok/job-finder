f188e2c152df60c31c60479896643b49f90e4dcb
J_Coder <seok3765@naver.com>
2023-11-14 04:35:24 +0900

    Real_Start_IntelliJ

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,55 @@
+import javax.swing.*;
+import java.awt.event.*;
 import java.sql.*;
-public class Main {
-    Connection con = null;
+
+public class Main extends JDialog {
+     Connection con = null;
     String url = "jdbc:oracle:thin:@minseok821lab.kro.kr:1521:orcl";
     String id = "seok3764";
     String pw = "0424";
-    public Main(){
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    public Main() {
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            System.out.println("드라이버 적재 성공");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("드라이버 적재 실패");
+        }
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // X 클릭 시 onCancel() 호출
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // ESCAPE 시 onCancel() 호출
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
             System.out.println("드라이버 적재 성공");
@@ -14,6 +59,15 @@ public class Main {
         }
     }
 
+    private void onOK() {
+        // 이곳에 코드 추가
+        dispose();
+    }
+
+    private void onCancel() {
+        // 필요한 경우 이곳에 코드 추가
+        dispose();
+    }
     private void DB_Connect(){
         try{
             con = DriverManager.getConnection(url,id,pw);
@@ -24,7 +78,7 @@ public class Main {
         }
     }
 
-    private void sqlRun()throws SQLException{
+    private void sqlRun()throws SQLException {
         String query = "select * from 개인회원";
         try {
             DB_Connect();
@@ -32,20 +86,21 @@ public class Main {
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 System.out.print(rs.getString("회원ID") + "\t" + rs.getString("비밀번호"));
-                System.out.print("\t" + rs.getInt(3) + "\n");
             }
             stmt.close();
             rs.close();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             con.close();
         }
     }
-    public static void main(String[] args) throws SQLException{
-        Main m = new Main();
-        m.sqlRun();
-    }
 
+    public static void main(String[] args) throws SQLException {
+        Main dialog = new Main();
+        dialog.sqlRun();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
 }
