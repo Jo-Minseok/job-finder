76424d53d443312b55847d4e4fe4f9e88550157a
J_Coder <seok3765@naver.com>
2023-11-14 14:50:26 +0900

    Init_Start_Main_Login
    회원가입 폼, 로그인 폼, 메인 코드 나눔
    DB 연결 및 쿼리문 정상 작동
    UI 정상 작동

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,55 +1,12 @@
-import javax.swing.*;
-import java.awt.event.*;
 import java.sql.*;
 
-public class Main extends JDialog {
-     Connection con = null;
+public class Main {
+    Connection con = null;
     String url = "jdbc:oracle:thin:@minseok821lab.kro.kr:1521:orcl";
     String id = "seok3764";
     String pw = "0424";
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
 
-    public Main() {
-        try{
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            System.out.println("드라이버 적재 성공");
-        }
-        catch(ClassNotFoundException e){
-            System.out.println("드라이버 적재 실패");
-        }
-        setContentPane(contentPane);
-        setModal(true);aaa
-        getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-        // X 클릭 시 onCancel() 호출
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-        // ESCAPE 시 onCancel() 호출
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-
+    public Main(){
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
             System.out.println("드라이버 적재 성공");
@@ -59,15 +16,6 @@ public class Main extends JDialog {
         }
     }
 
-    private void onOK() {
-        // 이곳에 코드 추가
-        dispose();
-    }
-
-    private void onCancel() {
-        // 필요한 경우 이곳에 코드 추가
-        dispose();
-    }
     private void DB_Connect(){
         try{
             con = DriverManager.getConnection(url,id,pw);
@@ -95,12 +43,12 @@ public class Main extends JDialog {
             con.close();
         }
     }
-
-    public static void main(String[] args) throws SQLException {
-        Main dialog = new Main();
-        dialog.sqlRun();
-        dialog.pack();
-        dialog.setVisible(true);
+    public static void main(String[] args) throws SQLException{
+        Main DB = new Main();
+        DB.sqlRun();
+        REGISTER test = new REGISTER();
+        test.pack();
+        test.setVisible(true);
         System.exit(0);
     }
 }
