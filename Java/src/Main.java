4067f4867ddaeadab69679dce3bc87fa684a3e25
J_Coder <seok3765@naver.com>
2023-11-14 12:56:47 +0900

    20231114

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -21,7 +21,7 @@ public class Main extends JDialog {
         }
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK); b
+        getRootPane().setDefaultButton(buttonOK);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
