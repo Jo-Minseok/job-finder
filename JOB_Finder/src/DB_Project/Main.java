243d86137bc1ae610039556dd0179b205e7f563d
J_Coder <seok3765@naver.com>
2023-11-22 01:41:56 +0900

    main DB Connection

--- JOB_Finder/src/DB_Project/Main.java
+++ JOB_Finder/src/DB_Project/Main.java
@@ -1,7 +1,13 @@
 package DB_Project;
 
-public class Main {
+import java.sql.*;
 
+public class Main {
+	static Connection con = null;
+	static String url = "jdbc:oracle:thin:@minseok821lab.kro.kr:1521:orcl";
+	static String id = "seok3764";
+	static String pw = "0424";
+	
 	public static void main(String[] args) {
 		try {
 			Login window = new Login();
@@ -11,6 +17,17 @@ public class Main {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("드라이버 적재 성공");
+			con = DriverManager.getConnection(url,id,pw);
+			System.out.println("데이터베이스 연결 성공");
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 찾기 실패");
+		} catch(SQLException e) {
+			System.out.println("연결에 실패");
+		}
+		
 	}
 
 }
