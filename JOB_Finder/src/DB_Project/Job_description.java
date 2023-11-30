52e4463f0d39ac260497ec436a4af95568ceb83d
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2023-11-30 18:29:12 +0900

    Add files via upload

---
+++ JOB_Finder/src/DB_Project/Job_description.java
@@ -0,0 +1,71 @@
+package DB_Project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Job_description {
+
+	public JFrame frame;
+	
+	public Job_description() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 225);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("<기업명>");
+		lblNewLabel.setBounds(12, 10, 410, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("<설명회명>");
+		lblNewLabel_1.setBounds(12, 35, 410, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("회차 : ");
+		lblNewLabel_2.setBounds(12, 60, 36, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("일시 : ");
+		lblNewLabel_3.setBounds(12, 85, 36, 15);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("장소 : ");
+		lblNewLabel_4.setBounds(12, 110, 36, 15);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setBounds(49, 60, 373, 15);
+		frame.getContentPane().add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("New label");
+		lblNewLabel_6.setBounds(49, 85, 373, 15);
+		frame.getContentPane().add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("New label");
+		lblNewLabel_7.setBounds(49, 110, 373, 15);
+		frame.getContentPane().add(lblNewLabel_7);
+		
+		JButton btnNewButton = new JButton("닫기");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton.setBounds(325, 153, 97, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
