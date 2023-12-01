0073cdb50e1eed7f60e506e251f18e3b81ac41d0
J_Coder <seok3765@naver.com>
2023-12-02 03:39:20 +0900

    Complete SQL

---
+++ Procedure.sql
@@ -0,0 +1,210 @@
+-----------------------------------------------------------------------------------------------
+------------------------------------------- 프로시저 -------------------------------------------
+-----------------------------------------------------------------------------------------------
+DROP PROCEDURE PASSWORD_PROTECTION_PERSONAL;
+DROP PROCEDURE PASSWORD_PROTECTION_BUSINESS;
+DROP PROCEDURE CREATE_ACCOUNT_PERSONAL;
+DROP PROCEDURE CREATE_ACCOUNT_BUSINESS;
+
+DBMS_SCHEDULER.DROP_JOB('매일_자정_이력서_삭제');
+DBMS_SCHEDULER.DROP_JOB('매일_자정_포지션_제안_삭제');
+DBMS_SCHEDULER.DROP_JOB('매일_자정_채용_게시글_삭제');
+DROP PROCEDURE DELETE_RESUME_DEADLINE;
+DROP PROCEDURE DELETE_POSITION_DEADLINE;
+DROP PROCEDURE DELETE_JOB_VACANCY;
+
+
+------------------------------------- [ID/PW 찾기 FROM] -----------------------------------------
+-- 개인회원
+CREATE OR REPLACE PROCEDURE PASSWORD_PROTECTION_PERSONAL(고객ID IN 개인회원.회원ID%TYPE, 부분비밀번호 OUT CHAR)
+AS
+BEGIN
+    SELECT RPAD(SUBSTR(비밀번호,1,3),LENGTH(비밀번호),'*') INTO 부분비밀번호 FROM 개인회원 WHERE 회원ID = 고객ID;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+        부분비밀번호 := '아이디가 존재하지 않습니다!';
+END;
+-- 기업회원
+CREATE OR REPLACE PROCEDURE PASSWORD_PROTECTION_BUSINESS(고객ID IN 개인회원.회원ID%TYPE, 부분비밀번호 OUT CHAR)
+AS
+BEGIN
+    SELECT RPAD(SUBSTR(비밀번호,1,3),LENGTH(비밀번호),'*') INTO 부분비밀번호 FROM 기업회원 WHERE 회원ID = 고객ID;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+        부분비밀번호 := '아이디가 존재하지 않습니다!';
+END;
+
+-------------------------------------- [회원가입 FROM] ------------------------------------------
+-- 개인 회원
+CREATE OR REPLACE PROCEDURE CREATE_ACCOUNT_PERSONAL(
+    회원이름 IN 개인회원.이름%TYPE,
+    휴대폰 IN 개인회원.휴대폰%TYPE,
+    회원ID IN 개인회원.회원ID%TYPE,
+    비밀번호 IN 개인회원.비밀번호%TYPE,
+    생년월일 IN 개인회원.생년월일%TYPE,
+    성별 IN 개인회원.성별%TYPE,
+    거주_지역 IN 개인회원.거주_지역%TYPE,
+    개인정보_유효기간 IN 개인회원.개인정보_유효기간%TYPE,
+    기업_이름 IN 개인회원.기업_이름%TYPE,
+    연봉 IN 개인회원.연봉%TYPE,
+    직책 IN 개인회원.직책%TYPE)
+AS
+    변환된_휴대폰 개인회원.휴대폰%TYPE;
+    삭제된_ID 개인회원.회원ID%TYPE;
+BEGIN
+    SELECT 회원ID INTO 삭제된_ID FROM 개인_회원_정보_변경  WHERE 개인_회원_정보_변경.변경_열 = '탈퇴' AND 개인_회원_정보_변경.회원ID = 회원ID;
+    IF(NOT SQL%NOTFOUND) THEN
+        RAISE_APPLICATION_ERROR(-20008, '이미 탈퇴한 회원입니다.');
+    END IF;
+    IF(LENGTH(휴대폰) = 11) THEN
+        BEGIN
+            변환된_휴대폰 := SUBSTR(휴대폰, 1,3) || '-' || SUBSTR(휴대폰, 4,4) || '-' || SUBSTR(휴대폰, 8,4);
+        END;
+    ELSIF(LENGTH(휴대폰) = 13) THEN
+        BEGIN
+            변환된_휴대폰 := 휴대폰;
+        END;
+    ELSE
+        BEGIN
+            RAISE_APPLICATION_ERROR(-20001, '전화번호의 입력이 올바르지 않습니다.');
+        END;
+    END IF;
+    
+    IF REGEXP_LIKE(회원ID,'\s') OR LENGTH(회원ID) <=6 OR NOT REGEXP_LIKE(회원ID,'[[:alpha:]]') OR NOT REGEXP_LIKE(회원ID,'[[:digit:]]') THEN
+        RAISE_APPLICATION_ERROR(-20002, '회원ID의 형식이 올바르지 않거나 ID 길이가 6자를 넘지 않습니다.');
+    END IF;
+    
+     IF REGEXP_LIKE(회원ID,'\s') OR LENGTH(회원ID) <=8 OR NOT REGEXP_LIKE(회원ID,'[[:alpha:]]') OR NOT REGEXP_LIKE(회원ID,'[[:digit:]]') THEN
+        RAISE_APPLICATION_ERROR(-20003, '비밀번호의 형식이 올바르지 않거나 길이가 8자를 넘지 않습니다.');
+    END IF;
+    
+    IF EXTRACT(YEAR FROM 생년월일) < EXTRACT(YEAR FROM SYSDATE) - 20 THEN
+        RAISE_APPLICATION_ERROR(-20004, '미성년자는 가입이 불가능합니다.'||EXTRACT(YEAR FROM SYSDATE)-20 ||' 이전 년생 부터 가입 가능.');
+    END IF;
+    
+    IF NOT (성별='남' OR 성별='여') THEN
+        RAISE_APPLICATION_ERROR(-20005, '성별이 올바르지 않습니다.(''남'',''여''로 입력하세요.)');
+    END IF;
+    INSERT INTO 개인회원 VALUES (회원ID, 비밀번호, 회원이름, 생년월일, 성별, 변환된_휴대폰, 거주_지역, 개인정보_유효기간, SYSDATE, 0, 0, 기업_이름, 연봉,직책);
+    
+END;
+-- 기업 회원
+CREATE OR REPLACE PROCEDURE CREATE_ACCOUNT_BUSINESS(
+    기업명 IN 기업회원.기업명%TYPE,
+    회원이름 IN 기업회원.이름%TYPE,
+    휴대폰 IN 기업회원.휴대폰%TYPE,
+    회원ID IN 기업회원.회원ID%TYPE,
+    비밀번호 IN 기업회원.비밀번호%TYPE,
+    개인정보유효기간 IN 기업회원.개인정보_유효기간%TYPE)
+AS
+    변환된_휴대폰 기업회원.휴대폰%TYPE;
+    삭제된_ID 기업회원.회원ID%TYPE;
+BEGIN   
+    SELECT 회원ID INTO 삭제된_ID FROM 기업_회원_정보_변경 WHERE 기업_회원_정보_변경.변경_열 = '탈퇴' AND 기업_회원_정보_변경.회원ID = 회원ID;
+    IF(NOT SQL%NOTFOUND) THEN
+        RAISE_APPLICATION_ERROR(-20008, '이미 탈퇴한 회원입니다.');
+    END IF;
+    IF(LENGTH(휴대폰) = 11) THEN
+        BEGIN
+            변환된_휴대폰 := SUBSTR(휴대폰, 1,3) || '-' || SUBSTR(휴대폰, 4,4) || '-' || SUBSTR(휴대폰, 8,4);
+        END;
+    ELSIF(LENGTH(휴대폰) = 13) THEN
+        BEGIN
+            변환된_휴대폰 := 휴대폰;
+        END;
+    ELSE
+        BEGIN
+            RAISE_APPLICATION_ERROR(-20001, '전화번호의 입력이 올바르지 않습니다.');
+        END;
+    END IF;
+    
+    IF REGEXP_LIKE(회원ID,'\s') OR LENGTH(회원ID) <=6 OR NOT REGEXP_LIKE(회원ID,'[[:alpha:]]') OR NOT REGEXP_LIKE(회원ID,'[[:digit:]]') THEN
+        RAISE_APPLICATION_ERROR(-20002, '회원ID의 형식이 올바르지 않거나 ID 길이가 6자를 넘지 않습니다.');
+    END IF;
+    
+     IF REGEXP_LIKE(회원ID,'\s') OR LENGTH(회원ID) <=8 OR NOT REGEXP_LIKE(회원ID,'[[:alpha:]]') OR NOT REGEXP_LIKE(회원ID,'[[:digit:]]') THEN
+        RAISE_APPLICATION_ERROR(-20003, '비밀번호의 형식이 올바르지 않거나 길이가 8자를 넘지 않습니다.');
+    END IF;
+END;
+-------------------------------------- [이력서 조회 FROM] ------------------------------------------
+CREATE OR REPLACE PROCEDURE COMPITITION_RATE
+AS
+    POST_NUMBER 채용_게시글.게시글_번호%TYPE;
+    APPLY_COUNT NUMBER(38,0);
+    CURSOR CURSOR_COMPITION_RATE IS SELECT 게시글_번호, COUNT(게시글_번호) FROM 지원 GROUP BY 게시글_번호;
+BEGIN
+    OPEN CURSOR_COMPITION_RATE;
+    LOOP
+        FETCH CURSOR_COMPITION_RATE INTO POST_NUMBER, APPLY_COUNT;
+        EXIT WHEN CURSOR_COMPITION_RATE%NOTFOUND;
+        UPDATE 채용_게시글 SET 경쟁률 = (SELECT 모집인원 FROM 채용_게시글 WHERE 게시글_번호 = POST_NUMBER)/APPLY_COUNT * 100 WHERE 게시글_번호 = POST_NUMBER;
+    END LOOP;
+    CLOSE CURSOR_COMPITION_RATE;
+END;
+
+-----------------------------------------------------------------------------------------------
+------------------------------------------- 스케줄러 -------------------------------------------
+-----------------------------------------------------------------------------------------------
+BEGIN
+    DBMS_SCHEDULER.CREATE_JOB(
+        -- 
+        JOB_NAME => '매일_자정_이력서_삭제',
+        JOB_TYPE => 'PLSQL_BLOCK',
+        JOB_ACTION => 'BEGIN DELETE_RESUME_DEADLINE; END;',
+        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
+        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
+        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
+        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
+        END_DATE => NULL,
+        ENABLED => TRUE
+    );
+    
+    DBMS_SCHEDULER.CREATE_JOB(
+        -- 
+        JOB_NAME => '매일_자정_포지션_제안_삭제',
+        JOB_TYPE => 'PLSQL_BLOCK',
+        JOB_ACTION => 'BEGIN DELETE_POSITION_DEADLINE; END;',
+        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
+        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
+        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
+        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
+        END_DATE => NULL,
+        ENABLED => TRUE
+    );
+    
+    DBMS_SCHEDULER.CREATE_JOB(
+        -- 
+        JOB_NAME => '매일_자정_채용_게시글_삭제',
+        JOB_TYPE => 'PLSQL_BLOCK',
+        JOB_ACTION => 'BEGIN DELETE_JOB_VACANCY; END;',
+        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
+        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
+        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
+        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
+        END_DATE => NULL,
+        ENABLED => TRUE
+    );
+END;
+
+------------------------------------ [스케쥴러 + 프로시저] ------------------------------------------
+-- 이력서 삭제 프로시저
+CREATE OR REPLACE PROCEDURE DELETE_RESUME_DEADLINE
+AS
+    현재_날짜 DATE := SYSDATE;
+BEGIN
+    DELETE FROM 이력서 WHERE 작성일자 < (현재_날짜 - INTERVAL '3' YEAR);
+END;
+
+-- 포지션 제안 삭제 프로시저
+CREATE OR REPLACE PROCEDURE DELETE_POSITION_DEADLINE
+AS
+    현재_날짜 DATE := SYSDATE;
+BEGIN
+    DELETE FROM 포지션_제안 WHERE 마감기한 < 현재_날짜;
+END;
+
+-- 채용 게시글 삭제 프로시저
+CREATE OR REPLACE PROCEDURE DELETE_JOB_VACANCY
+AS
+    현재_날짜 DATE := SYSDATE;
+BEGIN
+    DELETE FROM 채용_게시글 WHERE 마감일 < 현재_날짜;
+END;
--- TableCreation.sql
+++ TableCreation.sql
@@ -31,6 +31,20 @@ DROP SEQUENCE 채용_게시글번호_SEQ;
 -----------------------------------------------------------------------------------------------
 
 -- 강성 개체
+CREATE TABLE 기업(
+ 이름 NVARCHAR2(60),
+ 산업 NVARCHAR2(60),
+ 기업구분 NVARCHAR2(20),
+ 자본금 NUMBER(38,0),
+ 대표자 NVARCHAR2(60),
+ 사원수 NUMBER(5,0),
+ 설립일 DATE,
+ 매출액 NUMBER(38,0),
+ 대졸초임 NUMBER(38,0),
+ 지역 NCHAR(6),
+ CONSTRAINT PK_기업 PRIMARY KEY(이름)
+);
+
 CREATE TABLE 개인회원(
     회원ID NVARCHAR2(32),
     비밀번호 NVARCHAR2(32),
@@ -47,21 +61,8 @@ CREATE TABLE 개인회원(
     연봉 NUMBER(38,0),
     직책 NCHAR(6),
     CONSTRAINT PK_개인회원 PRIMARY KEY(회원ID),
-    CONSTRAINT UK_개인회원 UNIQUE (휴대폰)
-);
-
-CREATE TABLE 기업(
- 이름 NVARCHAR2(60),
- 산업 NVARCHAR2(60),
- 기업구분 NVARCHAR2(20),
- 자본금 NUMBER(38,0),
- 대표자 NVARCHAR2(60),
- 사원수 NUMBER(5,0),
- 설립일 DATE,
- 매출액 NUMBER(38,0),
- 대졸초임 NUMBER(38,0),
- 지역 NCHAR(6),
- CONSTRAINT PK_기업 PRIMARY KEY(이름)
+    CONSTRAINT UK_개인회원 UNIQUE (휴대폰),
+    CONSTRAINT FK_개인회원 FOREIGN KEY (기업_이름) REFERENCES 기업(이름) ON DELETE SET NULL
 );
 
 CREATE TABLE 기업회원(
@@ -76,7 +77,7 @@ CREATE TABLE 기업회원(
     포인트 NUMBER(38,0),
     게시글_작성수 NUMBER(38,0),
     CONSTRAINT PK_기업회원 PRIMARY KEY(회원ID),
-    CONSTRAINT FK_기업회원 FOREIGN KEY (기업명) REFERENCES 기업(이름)
+    CONSTRAINT FK_기업회원 FOREIGN KEY (기업명) REFERENCES 기업(이름) ON DELETE CASCADE
 );
 
 -- 약성 개체
@@ -88,7 +89,7 @@ CREATE TABLE 이력서(
     해외_경험_횟수 NUMBER(2,0),
     작성일자 DATE,
     CONSTRAINT PK_이력서 PRIMARY KEY (작성자ID, 이력서명),
-    CONSTRAINT FK_이력서 FOREIGN KEY(작성자ID) REFERENCES 개인회원(회원ID)
+    CONSTRAINT FK_이력서 FOREIGN KEY(작성자ID) REFERENCES 개인회원(회원ID) ON DELETE CASCADE
 );
 
 CREATE TABLE 채용_게시글(
@@ -103,8 +104,9 @@ CREATE TABLE 채용_게시글(
     모집인원 NUMBER(38,0),
     직책 NCHAR(6),
     마감일 DATE,
+    경쟁률 NUMBER(3,0),
     CONSTRAINT PK_채용_게시글 PRIMARY KEY(게시글_번호),
-    CONSTRAINT FK_채용_게시글 FOREIGN KEY (작성자ID) REFERENCES 기업회원(회원ID)
+    CONSTRAINT FK_채용_게시글 FOREIGN KEY (작성자ID) REFERENCES 기업회원(회원ID) ON DELETE CASCADE
 );
 
 CREATE TABLE 채용_설명회(
@@ -114,7 +116,7 @@ CREATE TABLE 채용_설명회(
     일시 DATE,
     장소 NVARCHAR2(60),
     CONSTRAINT PK_채용_설명회 PRIMARY KEY(기업명, 설명회명, 회차),
-    CONSTRAINT FK_채용_설명회 FOREIGN KEY (기업명) REFERENCES 기업(이름)
+    CONSTRAINT FK_채용_설명회 FOREIGN KEY (기업명) REFERENCES 기업(이름) ON DELETE CASCADE
 );
 
 -- 다중값 테이블
@@ -123,7 +125,7 @@ CREATE TABLE 이력서_자격증(
     이력서명 NVARCHAR2(40),
     자격증명 NVARCHAR2(32),
     CONSTRAINT PK_이력서_자격증 PRIMARY KEY(이력서_작성자,이력서명,자격증명),
-    CONSTRAINT FK_이력서_자격증 FOREIGN KEY(이력서_작성자,이력서명) REFERENCES 이력서(작성자ID,이력서명)
+    CONSTRAINT FK_이력서_자격증 FOREIGN KEY(이력서_작성자,이력서명) REFERENCES 이력서(작성자ID,이력서명) ON DELETE CASCADE
 );
 
 CREATE TABLE 이력서_경력(
@@ -134,7 +136,7 @@ CREATE TABLE 이력서_경력(
     직급 NCHAR(6),
     연봉 NUMBER(11,0),
     CONSTRAINT PK_이력서_경력 PRIMARY KEY (회원ID,이력서명,경력_위치),
-    CONSTRAINT FK_이력서_경력 FOREIGN KEY (회원ID, 이력서명) REFERENCES 이력서(작성자ID, 이력서명)
+    CONSTRAINT FK_이력서_경력 FOREIGN KEY (회원ID, 이력서명) REFERENCES 이력서(작성자ID, 이력서명) ON DELETE CASCADE
 );
 
 -- 관계 테이블
@@ -143,8 +145,8 @@ CREATE TABLE 포지션_제안(
     이력서_작성자 NVARCHAR2(32),
     이력서명 NVARCHAR2(40),
     마감기한 DATE,
-    CONSTRAINT FK_포지션_제안_제안자 FOREIGN KEY(제안자) REFERENCES 기업회원(회원ID),
-    CONSTRAINT FK_포지션_제안_이력서 FOREIGN KEY(이력서_작성자, 이력서명) REFERENCES 이력서(작성자ID, 이력서명),
+    CONSTRAINT FK_포지션_제안_제안자 FOREIGN KEY(제안자) REFERENCES 기업회원(회원ID) ON DELETE CASCADE,
+    CONSTRAINT FK_포지션_제안_이력서 FOREIGN KEY(이력서_작성자, 이력서명) REFERENCES 이력서(작성자ID, 이력서명) ON DELETE CASCADE,
     CONSTRAINT PK_포지션_제안 PRIMARY KEY(제안자, 이력서_작성자,이력서명)
 );
 
@@ -154,8 +156,8 @@ CREATE TABLE 지원(
     이력서명 NVARCHAR2(40),
     일시정보 DATE,
     합격_여부 CHAR(1),
-    CONSTRAINT FK_지원_채용게시글 FOREIGN KEY (게시글_번호) REFERENCES 채용_게시글(게시글_번호),
-    CONSTRAINT FK_지원_이력서 FOREIGN KEY (지원자, 이력서명) REFERENCES 이력서(작성자ID, 이력서명),
+    CONSTRAINT FK_지원_채용게시글 FOREIGN KEY (게시글_번호) REFERENCES 채용_게시글(게시글_번호) ON DELETE CASCADE,
+    CONSTRAINT FK_지원_이력서 FOREIGN KEY (지원자, 이력서명) REFERENCES 이력서(작성자ID, 이력서명) ON DELETE CASCADE,
     CONSTRAINT PK_지원 PRIMARY KEY(게시글_번호,지원자,이력서명)
 );
 
@@ -169,173 +171,32 @@ CREATE TABLE 개인_포인트_수정_내역(
     회원ID NVARCHAR2(32),
     내역 NCHAR(40),
     포인트 NUMBER(38,0),
-    CONSTRAINT FK_개인_포인트수정내역 FOREIGN KEY (회원ID) REFERENCES 개인회원(회원ID)
+    CONSTRAINT FK_개인_포인트수정내역 FOREIGN KEY (회원ID) REFERENCES 개인회원(회원ID) ON DELETE CASCADE
 );
 
 CREATE TABLE 기업_포인트_수정_내역(
     회원ID NVARCHAR2(32),
     내역 NCHAR(40),
     포인트 NUMBER(38,0),
-    CONSTRAINT FK_기업_포인트수정내역 FOREIGN KEY (회원ID) REFERENCES 기업회원(회원ID)
+    CONSTRAINT FK_기업_포인트수정내역 FOREIGN KEY (회원ID) REFERENCES 기업회원(회원ID) ON DELETE CASCADE
 );
 
 CREATE TABLE 개인_회원_정보_변경(
     회원ID NVARCHAR2(32),
     변경_열 NCHAR(40),
-    변경_데이터 NVARCHAR2(76),
-    CONSTRAINT FK_개인_정보_수정내역 FOREIGN KEY (회원ID) REFERENCES 기업회원(회원ID)
+    변경_데이터 NVARCHAR2(76)
 );
 
 CREATE TABLE 기업_회원_정보_변경(
     회원ID NVARCHAR2(32),
     변경_열 NCHAR(40),
-    변경_데이터 NVARCHAR2(76),
-    CONSTRAINT FK_기업_정보_수정내역 FOREIGN KEY (회원ID) REFERENCES 기업회원(회원ID)
+    변경_데이터 NVARCHAR2(76)
 );
 
 CREATE TABLE 연봉_평균_계산(
     기업명 NVARCHAR2(60),
     직책 NCHAR(6),
     평균 NUMBER(38,0),
-    CONSTRAINT FK_연봉_평균_계산 FOREIGN KEY (기업명) REFERENCES 기업(이름),
+    CONSTRAINT FK_연봉_평균_계산 FOREIGN KEY (기업명) REFERENCES 기업(이름) ON DELETE CASCADE,
     CONSTRAINT PK_연봉_평균_계산 PRIMARY KEY (기업명, 직책)
 );
------------------------------------------------------------------------------------------------
--------------------------------------------- 시퀀스 --------------------------------------------
------------------------------------------------------------------------------------------------
--- 시퀀스: 게시글 번호 자동 부여
-CREATE SEQUENCE 채용_게시글번호_SEQ
-START WITH 1
-INCREMENT BY 1
-MINVALUE 1
-MAXVALUE 99999999
-CYCLE;
-
------------------------------------------------------------------------------------------------
--------------------------------------------- 트리거 --------------------------------------------
------------------------------------------------------------------------------------------------
--- 시퀀스: 게시글 번호 자동 부여
-CREATE OR REPLACE TRIGGER 채용_게시글번호_SEQ_TRIG BEFORE INSERT ON 채용_게시글
-FOR EACH ROW
-BEGIN
-    SELECT 채용_게시글번호_SEQ.NEXTVAL INTO :NEW.게시글_번호 FROM DUAL;
-END;
-
--- 회원 정보 수정 평균 연봉 재연산 및 정보 변경 내역 저장
-
--- 채용/설명회 게시글 작성
-
--- 포인트 충전 및 사용
-
--- 회원 탈퇴
------------------------------------------------------------------------------------------------
-------------------------------------------- 프로시저 -------------------------------------------
------------------------------------------------------------------------------------------------
-------------------------------------- [ID/PW 찾기 FROM] -----------------------------------------
--- 개인회원
-CREATE OR REPLACE PROCEDURE PASSWROD_PROTECTION_PERSONAL(고객ID IN 개인회원.회원ID%TYPE, 부분비밀번호 OUT CHAR)
-AS
-BEGIN
-    SELECT RPAD(SUBSTR(비밀번호,1,3),LENGTH(비밀번호),'*') INTO 부분비밀번호 FROM 개인회원 WHERE 회원ID = 고객ID;
-    EXCEPTION WHEN NO_DATA_FOUND THEN
-        부분비밀번호 := '아이디가 존재하지 않습니다!';
-END;
--- 기업회원
-CREATE OR REPLACE PROCEDURE PASSWROD_PROTECTION_BUSINESS(고객ID IN 개인회원.회원ID%TYPE, 부분비밀번호 OUT CHAR)
-AS
-BEGIN
-    SELECT RPAD(SUBSTR(비밀번호,1,3),LENGTH(비밀번호),'*') INTO 부분비밀번호 FROM 기업회원 WHERE 회원ID = 고객ID;
-    EXCEPTION WHEN NO_DATA_FOUND THEN
-        부분비밀번호 := '아이디가 존재하지 않습니다!';
-END;
-
--------------------------------------- [회원가입 FROM] ------------------------------------------
--- 개인 회원
-CREATE OR REPLACE PROCEDURE CREATE_ACCOUNT_PERSONAL(
-    회원이름 IN 개인회원.이름%TYPE,
-    전화번호 IN 개인회원.전화번호%TYPE,
-    회원ID IN 개인회원.회원ID%TYPE,
-    비밀번호 IN 개인회원.비밀번호%TYPE,
-    생년월일 IN 개인회원.생년월일%TYPE,
-    성별 IN 개인회원.성별%TYPE)
-AS
-    변환된_전화번호 개인회원.전화번호%TYPE;
-BEGIN   
-
-    IF((SELECT LENGTH(전화번호) FROM DUAL) != 13) THEN
-        BEGIN
-            변환된_전화번호 := SUBSTR(전화번호, 1,3) || '-' || SUBSTR(전화번호, 4,4) || '-' || SUBSTR(전화번호, 8,4);
-    RETURN '회원가입 완료';
-END;
--- 기업 회원
-CREATE OR REPLACE PROCEDURE CREATE_ACCOUNT_BUSINESS()
-AS
-BEGIN
-END;
--------------------------------------- [이력서 조회 FROM] ------------------------------------------
-CREATE OR REPLACE PROCEDURE 
------------------------------------- [스케쥴러 + 프로시저] ------------------------------------------
--- 이력서 삭제 프로시저
-CREATE OR REPLACE PROCEDURE DELETE_RESUME_DEADLINE()
-AS
-    현재_날짜 DATE := SYSDATE;
-BEGIN
-    DELETE FROM 이력서 WHERE 작성일자 < (현재_날짜 - INTERVAL '3' YEAR);
-END;
-
-CREATE OR REPLACE PROCEDURE DELETE_POSITION_DEADLINE()
-AS
-    현재_날짜 DATE := SYSDATE;
-BEGIN
-    DELETE FROM 포지션_제안 WHERE 마감기한 < 현재_날짜;
-END;
-
-CREATE OR REPLACE PROCEDURE DELETE_JOB_VACANCY()
-AS
-    현재_날짜 DATE := SYSDATE;
-BEGIN
-    DELETE FROM WHERE 마감일 < 현재_날짜;
-END;
------------------------------------------------------------------------------------------------
-------------------------------------------- 스케줄러 -------------------------------------------
------------------------------------------------------------------------------------------------
-BEGIN
-    DBMS_SCHEDULER.CREATE_JOB(
-        -- 
-        JOB_NAME => '매일_자정_이력서_삭제',
-        JOB_TYPE => 'PLSQL_BLOCK',
-        JOB_ACTION => 'BEGIN DELETE_RESUME_DEADLINE; END;',
-        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
-        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
-        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
-        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
-        END_DATE => NULL,
-        ENABLED => TRUE
-    );
-    
-    DBMS_SCHEDULER.CREATE_JOB(
-        -- 
-        JOB_NAME => '매일_자정_포지션_제안_삭제',
-        JOB_TYPE => 'PLSQL_BLOCK',
-        JOB_ACTION => 'BEGIN DELETE_POSITION_DEADLINE; END;',
-        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
-        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
-        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
-        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
-        END_DATE => NULL,
-        ENABLED => TRUE
-    );
-    
-    DBMS_SCHEDULER.CREATE_JOB(
-        -- 
-        JOB_NAME => '매일_자정_채용_게시글_삭제',
-        JOB_TYPE => 'PLSQL_BLOCK',
-        JOB_ACTION => 'BEGIN DELETE_JOB_VACANCY; END;',
-        -- START_DATE : 스케쥴이 작동을 시작 할 시각. 입력한 시점부터 스케쥴러가 시작된다. AM 00시로 설정함
-        START_DATE => TRUNC(SYSDATE) + INTERVAL '1' DAY,
-        -- REPEAT_INTERVAL => 스케쥴이 작동하는 주기. 하루 한번 돌게 설정.
-        REPEAT_INTERVAL => 'FREQ=DAILY; BYHOUR=0; BYMINUTE=0; BYSECOND=0',
-        END_DATE => NULL,
-        ENABLED => TRUE
-    );
-END;
