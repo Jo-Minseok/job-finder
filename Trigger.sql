d795698b7ca5d0571b5ed338ca6b45c9324c6984
J_Coder <seok3765@naver.com>
2023-12-04 05:23:17 +0900

    trigger

--- Trigger.sql
+++ Trigger.sql
@@ -1,10 +1,10 @@
 -----------------------------------------------------------------------------------------------
 -------------------------------------------- 트리거 --------------------------------------------
 -----------------------------------------------------------------------------------------------
-DROP TRIGGER 개인_회원정보수정_TRIG;
-DROP TRIGGER 개인_회원정보수정_예외_TRIG;
+DROP TRIGGER 개인_회원정보수정_TRIG_AFTER;
+DROP TRIGGER 개인_회원정보수정_TRIG_BEFORE;
+
 DROP TRIGGER 기업_회원정보수정_TRIG;
-DROP TRIGGER 기업_회원정보수정_예외_TRIG;
 DROP TRIGGER 채용게시글_TRIG;
 DROP TRIGGER 채용설명회_TRIG;
 DROP TRIGGER 개인_포인트_변경_TRIG;
@@ -17,7 +17,7 @@ DROP SEQUENCE POST_NUMBER_SEQ;
 DROP TRIGGER POST_NUMBER_TRIG;
 
 -- 회원 정보 수정 평균 연봉 재연산 및 정보 변경 내역 저장
-CREATE OR REPLACE TRIGGER 개인_회원정보수정_TRIG AFTER UPDATE ON 개인회원
+CREATE OR REPLACE TRIGGER 개인_회원정보수정_TRIG_AFTER AFTER UPDATE ON 개인회원
 FOR EACH ROW
 DECLARE
 TMP_연봉 NUMBER;
@@ -32,97 +32,95 @@ BEGIN
     END IF;
 END 개인_회원정보수정_TRIG;
     
-
-CREATE OR REPLACE TRIGGER 개인_회원정보수정_TRIG BEFORE UPDATE ON 개인회원
+CREATE OR REPLACE TRIGGER 개인_회원정보수정_TRIG_BEFORE BEFORE UPDATE ON 개인회원
 FOR EACH ROW
-    BEGIN
-        IF UPDATING('휴대폰') THEN
-            BEGIN   
-                IF LENGTH(:NEW.휴대폰) = 11 THEN
-                    BEGIN
-                        :NEW.휴대폰 := SUBSTR(:NEW.휴대폰, 1,3) || '-' || SUBSTR(:NEW.휴대폰, 4,4) || '-' || SUBSTR(:NEW.휴대폰, 8,4);
-                    END;
-                ELSIF LENGTH(:NEW.휴대폰) <> 13 THEN
-                    BEGIN
-                        RAISE_APPLICATION_ERROR(-20001, '휴대폰 번호의 입력이 올바르지 않습니다.');
-                    END;
-                END IF;
-            END;
-        END IF;
+BEGIN
+    IF UPDATING('휴대폰') THEN
+        BEGIN   
+            IF LENGTH(:NEW.휴대폰) = 11 THEN
+                BEGIN
+                    :NEW.휴대폰 := SUBSTR(:NEW.휴대폰, 1,3) || '-' || SUBSTR(:NEW.휴대폰, 4,4) || '-' || SUBSTR(:NEW.휴대폰, 8,4);
+                END;
+            ELSIF LENGTH(:NEW.휴대폰) <> 13 THEN
+                BEGIN
+                    RAISE_APPLICATION_ERROR(-20001, '휴대폰 번호의 입력이 올바르지 않습니다.');
+                END;
+            END IF;
+        END;
+    END IF;
         
-        IF UPDATING('비밀번호') THEN
-            BEGIN
-                IF REGEXP_LIKE(:NEW.비밀번호,'\s') OR LENGTH(:NEW.비밀번호) <=8 OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:alpha:]]') OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:digit:]]') THEN
-                    BEGIN
-                        RAISE_APPLICATION_ERROR(-20003, '비밀번호의 형식이 올바르지 않거나 길이가 8자를 넘지 않습니다.');
-                    END;
-                END IF;
-            END;
-        END IF;
+    IF UPDATING('비밀번호') THEN
+        BEGIN
+            IF REGEXP_LIKE(:NEW.비밀번호,'\s') OR LENGTH(:NEW.비밀번호) <=8 OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:alpha:]]') OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:digit:]]') THEN
+                BEGIN
+                    RAISE_APPLICATION_ERROR(-20003, '비밀번호의 형식이 올바르지 않거나 길이가 8자를 넘지 않습니다.');
+                END;
+            END IF;
+        END;
+    END IF;
         
-        IF UPDATING('생년월일') THEN
-            BEGIN
-                IF EXTRACT(YEAR FROM :NEW.생년월일) > EXTRACT(YEAR FROM SYSDATE) - 20 THEN
-                    BEGIN
-                        RAISE_APPLICATION_ERROR(-20004, '미성년자로 수정이 불가능합니다. '|| TO_CHAR(EXTRACT(YEAR FROM SYSDATE)-20) ||'이전 년생으로 수정 가능.');
-                    END;
-                END IF;
-            END;
-        END IF;
+    IF UPDATING('생년월일') THEN
+        BEGIN
+            IF EXTRACT(YEAR FROM :NEW.생년월일) > EXTRACT(YEAR FROM SYSDATE) - 20 THEN
+                BEGIN
+                    RAISE_APPLICATION_ERROR(-20004, '미성년자로 수정이 불가능합니다. '|| TO_CHAR(EXTRACT(YEAR FROM SYSDATE)-20) ||'이전 년생으로 수정 가능.');
+                END;
+            END IF;
+        END;
+    END IF;
         
-        IF UPDATING('성별') THEN
-            BEGIN  
-                IF NOT (:NEW.성별='남' OR :NEW.성별='여') THEN
-                    BEGIN
-                        RAISE_APPLICATION_ERROR(-20005, '성별이 올바르지 않습니다.(''남'',''여''로 입력하세요.)');
-                    END;
-                END IF;
-            END;
-        END IF;
+    IF UPDATING('성별') THEN
+        BEGIN  
+            IF NOT (:NEW.성별='남' OR :NEW.성별='여') THEN
+                BEGIN
+                    RAISE_APPLICATION_ERROR(-20005, '성별이 올바르지 않습니다.(''남'',''여''로 입력하세요.)');
+                END;
+            END IF;
+        END;
+    END IF;
     
-        IF UPDATING('비밀번호') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '비밀번호', :NEW.비밀번호);
-            END;
-        END IF;
+    IF UPDATING('비밀번호') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '비밀번호', :NEW.비밀번호);
+        END;
+    END IF;
         
-        IF UPDATING('이름') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '이름', :NEW.이름);
-            END;
-        END IF;
+    IF UPDATING('이름') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '이름', :NEW.이름);
+        END;
+    END IF;
         
-        IF UPDATING('생년월일') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '생년월일', :NEW.생년월일);
-            END;
-        END IF;
+    IF UPDATING('생년월일') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '생년월일', :NEW.생년월일);
+        END;
+    END IF;
         
-        IF UPDATING('성별') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '성별', :NEW.성별);
-            END;
-        END IF;
+    IF UPDATING('성별') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '성별', :NEW.성별);
+        END;
+    END IF;
         
-        IF UPDATING('휴대폰') THEN
-            BEGIN   
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '휴대폰', :NEW.휴대폰);
-            END;
-        END IF;
+    IF UPDATING('휴대폰') THEN
+        BEGIN   
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '휴대폰', :NEW.휴대폰);
+        END;
+    END IF;
         
-        IF UPDATING('거주_지역') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '거주_지역', :NEW.거주_지역);
-            END;
-        END IF;
+    IF UPDATING('거주_지역') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '거주_지역', :NEW.거주_지역);
+        END;
+    END IF;
         
-        IF UPDATING('개인정보_유효기간') THEN
-            BEGIN
-                INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '개인정보_유효기간', :NEW.개인정보_유효기간);
-            END;
-        END IF;
+    IF UPDATING('개인정보_유효기간') THEN
+        BEGIN
+            INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '개인정보_유효기간', :NEW.개인정보_유효기간);
+        END;
+    END IF;
 END;
-
 
 CREATE OR REPLACE TRIGGER 기업_회원정보수정_TRIG BEFORE UPDATE ON 기업회원
 FOR EACH ROW
