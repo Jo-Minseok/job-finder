6a58de549c0f234d2cc7bb29d6f97d65cb0e64d9
Seo-aron <113684586+Seo-aron@users.noreply.github.com>
2023-12-06 00:12:55 +0900

    Market_trends.java

--- JOB_Finder/src/DB_Project/Market_trends.java
+++ JOB_Finder/src/DB_Project/Market_trends.java
@@ -1,117 +1,146 @@
 package DB_Project;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.GroupLayout.Alignment;
+public class Market_trends {
 
-public class Market_trends extends JFrame {
-
-	private static final long serialVersionUID = 1L;
+	public JFrame frame;
 	private JPanel contentPane;
-	private JTextField tf1;
-	private JTextField tf2;
-	private JTextField tf_start;
-	private JTextField tf_end;
-	
+	private JTextField txt_start;
+	private JTextField txt_end;
+	private JComboBox com_company;
+	private JButton btn_serch;
+
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Market_trends window = new Market_trends();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	public Market_trends() {
-		setTitle("시장 동향");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		setBounds(100, 100, 1200, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		initialize();
+	}
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 888, 745);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setLocation(10, 10);
-		panel.setSize(1162, 30);
-		contentPane.add(panel);
-		
-		JComboBox cb = new JComboBox();
-		cb.setBounds(0, 0, 120, 30);
-		cb.setModel(new DefaultComboBoxModel(new String[] {"전체", "회사A", "회사B", "회사C"}));
+		com_company = new JComboBox();
+		com_company.setBounds(0, 0, 192, 30);
+		CompanyComboBox();
 
-		//setContentPane(new GraphPanel());
-		setLayout(new BorderLayout());
-		Container c = getContentPane();
+		frame.setContentPane(new GraphPanel());
+		Container c = frame.getContentPane();
+		c.setLayout(null);
 		GraphPanel graphpanel = new GraphPanel();
-		c.add(graphpanel, BorderLayout.CENTER);
+		graphpanel.setBounds(0, 0, 0, 0);
+		c.add(graphpanel);
 		
-		tf_start = new JTextField();
-		tf_start.setBounds(130, 5, 120, 25);
-		tf_start.setText("시작일");
-		tf_start.setHorizontalAlignment(SwingConstants.CENTER);
-		tf_start.setColumns(10);
+		txt_start = new JTextField();
+		txt_start.setBounds(204, 3, 131, 25);
+		txt_start.setHorizontalAlignment(SwingConstants.CENTER);
+		txt_start.setColumns(10);
 		
 		JLabel lb1 = new JLabel("~");
 		lb1.setHorizontalAlignment(SwingConstants.CENTER);
-		lb1.setBounds(250, 5, 25, 25);
+		lb1.setBounds(344, 3, 25, 25);
 		
-		tf_end = new JTextField();
-		tf_end.setBounds(275, 5, 120, 25);
-		tf_end.setText("종료일");
-		tf_end.setHorizontalAlignment(SwingConstants.CENTER);
-		tf_end.setColumns(10);
+		txt_end = new JTextField();
+		txt_end.setBounds(381, 3, 131, 25);
+		txt_end.setHorizontalAlignment(SwingConstants.CENTER);
+		txt_end.setColumns(10);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(com_company);
+		frame.getContentPane().add(txt_start);
+		frame.getContentPane().add(lb1);
+		frame.getContentPane().add(txt_end);
 		
-		JButton bt_close = new JButton("닫기");
-		bt_close.setBounds(1087, 0, 75, 30);
-		panel.setLayout(null);
-		panel.add(cb);
-		panel.add(tf_start);
-		panel.add(lb1);
-		panel.add(tf_end);
-		panel.add(bt_close);
+		JButton btn_exit = new JButton("닫기");
+		btn_exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		btn_exit.setBounds(763, 10, 97, 23);
+		c.add(btn_exit);
 		
-		
+		btn_serch = new JButton("검색");
+		btn_serch.setBounds(524, 4, 97, 23);
+		c.add(btn_serch);
+		btn_serch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 	}
 	
-	class GraphPanel extends JPanel{
-		public void paintComponent(Graphics g) {
-			// 그래프 그리기
-			int v1=20, v2=60, v3=30, v4=10, v5=50, v6=40;
+		// 회사 콤보박스 DB연결
+		public ArrayList<String> getcompanyNameData(){
+			ArrayList<String> regionList = new ArrayList<>();
 			
-			g.clearRect(0, 0, getWidth(), getHeight());
+			try {	           
+				Main.DBConnection();
 
-			g.drawLine(50, 140, 50, 470);
-			
-			for(int i = 1; i<11; i++) {
-				g.drawString(i*10+"", 25, 475-(30*i));         // 10 ~ 100 표기
-				g.drawLine(50,  470-(30*i),  660, 470-(30*i)); // 10 마다 선 생성
-			}
-			g.drawLine(50,  470, 660,  470); 
-			
-			g.drawString("현재 채용중인 공고", 70, 490);
-			g.drawString("평균 연봉", 195, 490);
-			g.drawString("매출액", 295, 490);
-			g.drawString("옵션4", 405, 490);
-			g.drawString("옵션5", 505, 490);
-			g.drawString("옵션6", 605, 490);
-			
-			g.setColor(Color.BLUE);
-			if(v1>0)
-				g.fillRect(110, 470-v1*3, 20, v1*3); // 가로(20)크기의 막대 그래프 생성
-			if(v2>0)
-				g.fillRect(210, 470-v2*3, 20, v2*3);
-			if(v3>0)
-				g.fillRect(310, 470-v3*3, 20, v3*3);
-			if(v4>0)
-				g.fillRect(410, 470-v4*3, 20, v4*3);
-			if(v5>0)
-				g.fillRect(510, 470-v5*3, 20, v5*3);
-			if(v6>0)
-				g.fillRect(610, 470-v6*3, 20, v6*3);
+	            String query = "SELECT \"이름\" FROM \"SEOK3764\".\"기업\"";
+	            Main.stmt = Main.con.createStatement();
+	            Main.rs = Main.stmt.executeQuery(query);
+
+	            while (Main.rs.next()) {
+	                String companyName = Main.rs.getString("이름");
+	                regionList.add(companyName);
+	            }
+
+	            Main.rs.close();
+	            Main.stmt.close();
+	            Main.con.close();
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+
+	        return regionList;
+	    }
+		
+		// 회사 콤보박스 리스트 추가
+		public void CompanyComboBox() {
+		    ArrayList<String> companyNameList = getcompanyNameData();
+		    for (String companyName : companyNameList) {
+		        com_company.addItem(companyName);
+		    }
 		}
 	}
-}
+	
+	// 그래프 부분
+	class GraphPanel extends JPanel{
+		public void paintComponent(Graphics g) {
+			
+		}
+	}
+	
