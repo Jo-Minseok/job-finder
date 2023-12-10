c574aea2ee50ee2611763b44bf25ff4797c0dad4
J_Coder <seok3765@naver.com>
2023-12-11 04:47:27 +0900

    Trigger Edit_Info Refix

--- SQL/Trigger.sql
+++ SQL/Trigger.sql
@@ -65,7 +65,7 @@ DROP TRIGGER POST_NUMBER_TRIG;
 CREATE OR REPLACE TRIGGER 개인_회원정보수정_TRIG_BEFORE BEFORE UPDATE ON 개인회원
 FOR EACH ROW
 BEGIN
-    IF UPDATING('휴대폰') THEN
+    IF :NEW.휴대폰 <> :OLD.휴대폰 THEN
         BEGIN   
             IF LENGTH(:NEW.휴대폰) = 11 THEN
                 BEGIN
@@ -79,7 +79,7 @@ BEGIN
         END;
     END IF;
         
-    IF UPDATING('비밀번호') THEN
+    IF :NEW.비밀번호 <> :OLD.비밀번호 THEN
         BEGIN
             IF REGEXP_LIKE(:NEW.비밀번호,'\s') OR LENGTH(:NEW.비밀번호) <8 OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:alpha:]]') OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:digit:]]') THEN
                 BEGIN
@@ -89,7 +89,7 @@ BEGIN
         END;
     END IF;
         
-    IF UPDATING('생년월일') THEN
+    IF :NEW.생년월일 <> :OLD.생년월일 THEN
         BEGIN
             IF EXTRACT(YEAR FROM :NEW.생년월일) > EXTRACT(YEAR FROM SYSDATE) - 20 THEN
                 BEGIN
@@ -99,7 +99,7 @@ BEGIN
         END;
     END IF;
         
-    IF UPDATING('성별') THEN
+    IF :NEW.성별 <> :OLD.성별 THEN
         BEGIN  
             IF NOT (:NEW.성별='남' OR :NEW.성별='여') THEN
                 BEGIN
@@ -109,43 +109,43 @@ BEGIN
         END;
     END IF;
     
-    IF UPDATING('비밀번호') THEN
+    IF :NEW.비밀번호 <> :OLD.비밀번호 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '비밀번호', :NEW.비밀번호, :OLD.비밀번호);
         END;
     END IF;
         
-    IF UPDATING('이름') THEN
+    IF :NEW.이름 <> :OLD.이름 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '이름', :NEW.이름, :OLD.이름);
         END;
     END IF;
         
-    IF UPDATING('생년월일') THEN
+    IF :NEW.생년월일 <> :OLD.생년월일 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '생년월일', :NEW.생년월일, :OLD.생년월일);
         END;
     END IF;
         
-    IF UPDATING('성별') THEN
+    IF :NEW.성별 <> :OLD.성별 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '성별', :NEW.성별, :OLD.성별);
         END;
     END IF;
         
-    IF UPDATING('휴대폰') THEN
+    IF :NEW.휴대폰 <> :OLD.휴대폰 THEN
         BEGIN   
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '휴대폰', :NEW.휴대폰, :OLD.휴대폰);
         END;
     END IF;
         
-    IF UPDATING('거주_지역') THEN
+    IF :NEW.거주_지역 <> :OLD.거주_지역 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '거주_지역', :NEW.거주_지역, :OLD.거주_지역);
         END;
     END IF;
         
-    IF UPDATING('개인정보_유효기간') THEN
+    IF :NEW.개인정보_유효기간 <> :OLD.개인정보_유효기간 THEN
         BEGIN
             INSERT INTO 개인_회원_정보_변경 VALUES (:NEW.회원ID, '개인정보_유효기간', :NEW.개인정보_유효기간, :OLD.개인정보_유효기간);
         END;
@@ -155,7 +155,7 @@ END;
 CREATE OR REPLACE TRIGGER 기업_회원정보수정_TRIG BEFORE UPDATE ON 기업회원
 FOR EACH ROW
 BEGIN
-    IF UPDATING('휴대폰') THEN
+    IF :NEW.휴대폰 <> :OLD.휴대폰 THEN
         IF(LENGTH(:NEW.휴대폰) = 11) THEN
             BEGIN
                 :NEW.휴대폰 := SUBSTR(:NEW.휴대폰, 1,3) || '-' || SUBSTR(:NEW.휴대폰, 4,4) || '-' || SUBSTR(:NEW.휴대폰, 8,4);
@@ -167,22 +167,22 @@ BEGIN
         END IF;
     END IF;
     
-    IF UPDATING('기업회원.비밀번호') THEN
+    IF :NEW.비밀번호 <> :OLD.비밀번호 THEN
         IF REGEXP_LIKE(:NEW.비밀번호,'\s') OR LENGTH(:NEW.비밀번호) <8 OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:alpha:]]') OR NOT REGEXP_LIKE(:NEW.비밀번호,'[[:digit:]]') THEN
             RAISE_APPLICATION_ERROR(-20003, '비밀번호의 형식이 올바르지 않거나 길이가 8자를 넘지 않습니다.');
         END IF;
     END IF;
 
-    IF UPDATING('이름') THEN
+    IF :NEW.이름 <> :OLD.이름 THEN
         INSERT INTO 기업_회원_정보_변경 VALUES (:NEW.회원ID, '이름', :NEW.이름, :OLD.이름);
     END IF;
-    IF UPDATING('개인정보_유효기간') THEN
+    IF :NEW.개인정보_유효기간 <> :OLD.개인정보_유효기간 THEN
         INSERT INTO 기업_회원_정보_변경 VALUES (:NEW.회원ID, '개인정보_유효기간', :NEW.개인정보_유효기간, :OLD.개인정보_유효기간);
     END IF;
-    IF UPDATING('휴대폰') THEN
+    IF :NEW.휴대폰 <> :OLD.휴대폰 THEN
         INSERT INTO 기업_회원_정보_변경 VALUES (:NEW.회원ID, '휴대폰', :NEW.휴대폰, :OLD.휴대폰);
     END IF;
-    IF UPDATING('비밀번호') THEN
+    IF :NEW.비밀번호 <> :OLD.비밀번호 THEN
         INSERT INTO 기업_회원_정보_변경 VALUES (:NEW.회원ID, '비밀번호', :NEW.비밀번호, :OLD.비밀번호);
     END IF;
 END;
